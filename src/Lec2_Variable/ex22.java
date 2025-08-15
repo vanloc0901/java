fa46f8ac552f57d4e1424dae19bef1d082a53f7c
vanloc0901 <lan9802006@gmail.com>
2025-08-15 16:52:38 +0700

    Delete src directory

--- src/Lec2_Variable/ex22.java
+++
@@ -1,9 +0,0 @@
-package Lec2_variable;
-public class ex22 {
-    public static void main(String[] args) {
-        int x = 1;
-        int z = x- x-- - x++ + --x - x + x-- + x;
-        System.out.println("z = " + z);
-        System.out.println("x = " + x);
-    }
-}
