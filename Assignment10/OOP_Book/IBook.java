c1a1281d4bc0056de20d9c2bc5ba7469778b4970
vanloc0901 <lan9802006@gmail.com>
2025-09-18 15:28:31 +0700

    Update IBook.java

--- Assignment10/OOP_Book/IBook.java
+++ Assignment10/OOP_Book/IBook.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Assignment10.OOP_Book;
-
-/**
- *
- * @author Administrator
- */
 public interface IBook {
     void displayDetails();
     double calculatePrice();
+    void addBook();
+    boolean updateBookById(String id);
 }
